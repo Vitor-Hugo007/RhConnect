e9a0c30b5ed504ce31310fb461541c6a1ea3acf0
Cafe <cafedagalaxia@hotmail.com>
2025-10-18 01:25:16 -0300

    Adicionando rotas

--- projeto/src/main.jsx
+++ projeto/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+// Provedor de Rotas do react-router-dom
+import { RouterProvider } from 'react-router-dom';
+
+// Gerenciador de rotas
+import MyRouter from './MyRouter.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     {/* Faz o provedor utilizar as rotas que voce
+    definiu no MyRouter */}
+    <RouterProvider router={MyRouter}/>
   </StrictMode>,
 )
