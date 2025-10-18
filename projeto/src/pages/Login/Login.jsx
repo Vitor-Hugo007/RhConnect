aa6be149172589fa1a0f2921ea11c02882e58988
Cafe <cafedagalaxia@hotmail.com>
2025-10-18 03:14:09 -0300

    Atualizando e Criando Login

--- projeto/src/pages/Login/Login.jsx
+++ projeto/src/pages/Login/Login.jsx
@@ -1,12 +1,26 @@
+// Importando css
+import styles from './Login.module.css';
 
 
+// Importando hook para a Verificação de Login
 
+import { useVerLogin } from "../../hooks/useUsuarios.jsx"; 
+
+// Importando a função useform do pacote hook-form
+import { useForm } from "react-hook-form"
+
+// Importando useState para tratar de variáveis
+import { useState } from "react";
+
+// importação do Navigate para transitar entre as paginas
+// import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    <h1>Bem-Vindos</h1>
     return(
-        <>
-            <h1>Login</h1>
-        </>
+        <div>
+            
+        </div>
     );
 }
 
